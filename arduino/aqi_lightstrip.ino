88cbe1a0a0c6e213d442ef5fcddc31eadf764e23
overrider <admin@bsdguides.org>
2015-02-11 16:36:59 +0800

    Multiple improvements, added datafile_to_http

--- arduino/aqi_lightstrip.ino
+++ arduino/aqi_lightstrip.ino
@@ -3,6 +3,7 @@
 # receives an INT and displays that value onto the LED Strip
 # In my case the LED Strip has 60 LEDs attached to it, and i 
 # allow the Pollution index to be from 0 to 200;
+
 #include "LPD8806.h"
 #include "SPI.h"
 
